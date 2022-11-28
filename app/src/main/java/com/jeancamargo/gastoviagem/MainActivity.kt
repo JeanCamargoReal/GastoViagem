6eaac7c409d0d0281ae1795535327d8f648a811f
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-11-28 15:50:35 -0300

    ViewBinding

--- app/src/main/java/com/jeancamargo/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/jeancamargo/gastoviagem/MainActivity.kt
@@ -2,10 +2,14 @@ package com.jeancamargo.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jeancamargo.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
