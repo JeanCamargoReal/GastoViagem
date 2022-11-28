2dc524aa571802b87d856b3f8847dc650a38a381
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-11-28 19:13:28 -0300

    Evento de click

--- app/src/main/java/com/jeancamargo/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/jeancamargo/gastoviagem/MainActivity.kt
@@ -2,14 +2,29 @@ package com.jeancamargo.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.jeancamargo.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        // Toast notification
+        Toast.makeText(this, "Fui clicado!", Toast.LENGTH_SHORT).show()
     }
 }
